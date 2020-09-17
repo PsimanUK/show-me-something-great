d5bb157e24c8ae6e838860096f792806e23dfb62
Simeon Parris <simeonparris@hotmail.com>
2020-09-17 11:29:21 +0100

    add basic movie component.

---
+++ src/components/Movie.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Movie = () => {
+    return (
+        <section>
+            <img src="images/placeholder.jpg" alt="Placeholder image" />
+            <h3>MOVIE TITLE</h3> <h4>(MOVIE YEAR)</h4>
+            <h5>GENRE</h5>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+};
+
+export default Movie;
